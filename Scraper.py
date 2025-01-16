7077891aec5ecc42cb94040184f718964080e33f
hi101000 <60079731+hi101000@users.noreply.github.com>
2025-01-16 15:39:24 -0500

    updated sources

--- Scraper.py
+++ Scraper.py
@@ -1,6 +1,6 @@
 import requests
 
-for i in range(1, 102):
-    cont = requests.get(f"https://www.1000dokumente.de/images/9/93/0138_gpo_{i}.jpg?download").content
+for i in range(19, 34):
+    cont = requests.get(f"https://s3.amazonaws.com/NARAprodstorage/lz/dc-metro/rg-242/7788344_T175/T175/T175-258A/T175-258-00{i}.jpg").content
     with open('./static/Images/Image_'+str(i)+'.jpg', 'wb') as f:
         f.write(cont)
