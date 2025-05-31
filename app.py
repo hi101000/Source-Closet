1ef14ca4dd858c20721036f05243163adbc26430
hi101000 <60079731+hi101000@users.noreply.github.com>
2025-05-31 09:35:29 -0400

    fixed build error and updated a source description

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ def sourceno(ip, city, region, country, timezone, loc, provider):
 def sitemap():
     return send_from_directory(app.static_folder, 'assets/sitemap.txt')
 
-@pp.route('/robots.txt')
+@app.route('/robots.txt')
 def robots():
     return send_from_directory(app.static_folder, 'assets/robots.txt')
 
