279c1136ff4060454d0c4db6f1b6a0b8995594e2
hi101000 <60079731+hi101000@users.noreply.github.com>
2025-12-07 12:59:57 -0500

    Test commit to see if this prompts a build

--- Scraper.py
+++ Scraper.py
@@ -5,9 +5,25 @@ for i in range(19, 34):
     with open('./static/Images/Image_'+str(i)+'.jpg', 'wb') as f:
         f.write(cont)'''
 
-from collections import Counter
+'''from collections import Counter
 
 c1 = Counter(["hi", "hello"])
 c2 = Counter(["hola", "hi", "bye", "hello"])
 
-print(c1-c2 == Counter())
+print(c1-c2 == Counter())'''
+
+'''import libsql
+
+table = "TAGS_FURTHER"
+conn_local = libsql.connect('static/sources.db')
+cur_local = conn_local.cursor()
+
+for row in cur_local.execute(f"SELECT * FROM {table}").fetchall():
+    tag_exists = cur_local.execute("SELECT 1 FROM TAGS WHERE ID = ?", (row[1],)).fetchone()
+    furt_exists = cur_local.execute("SELECT 1 FROM FURTHER WHERE ID = ?", (row[2],)).fetchone()
+    if not tag_exists or not furt_exists:
+        print(f"Invalid foreign key reference in row: {row}")
+        if not tag_exists:
+            print(f"  - TAG_ID {row[1]} does not exist in USERS table.")
+        if not furt_exists:
+            print(f"  - FURT_ID {row[2]} does not exist in SOURCES table.")'''
